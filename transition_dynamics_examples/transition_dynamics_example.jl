cacec9a907ec7686fb3cc6ed3a908220000b7efc
Jesse Perla <jesseperla@gmail.com>
2018-05-17 13:25:15 -0700

    Rearranged code and examples
    #25 tests in progress

--- transition_dynamics_examples/transition_dynamics_example.jl
+++ transition_dynamics_examples/transition_dynamics_example.jl
@@ -1,12 +1,12 @@
-using DifferentialEquations
+using DifferentialEquations, NamedTuples, BenchmarkTools
 
-function time_stepping_example(x_min, x_max, M, T, N, rho, sigma_bar, algorithm)
+function time_stepping_example(c_tilde, sigma_tilde, mu_tilde, x_min, x_max, M, T, rho, algorithm, dt=0.0001, adaptive=true)
+
+    #Setting up the general discretization matrices for the upwind L_1+ and the L_2 central differences
+    #These would be automaitcally generated from the DIffEqOperators.jl library
     M_bar = M + 2
     x = linspace(x_min, x_max, M)
     Delta = x[2]-x[1]
-    mu_tilde_T = -0.1+T+0.1*x
-    sigma_tilde_T = sigma_bar*x
-    c_tilde = exp.(x)
 
     L_1_plus = Tridiagonal(zeros(M_bar-1), -1 * ones(M_bar), ones(M_bar-1))[2: end-1, :]/Delta
     L_1_plus[:, 2] = L_1_plus[:, 2] + L_1_plus[:, 1]
@@ -18,36 +18,44 @@ function time_stepping_example(x_min, x_max, M, T, N, rho, sigma_bar, algorithm)
     L_2[:, end-1] = L_2[:, end-1] + L_2[:, end]
     L_2 = L_2[:, 2: end-1]
 
-    L_tilde = Diagonal(rho*ones(M)) - (Diagonal(mu_tilde_T)*L_1_plus + Diagonal(sigma_tilde_T.^2/2)*L_2)
-
-
-    u_T = L_tilde\c_tilde
+    #Calculating the stationary solution
+    @assert minimum(mu_tilde.(T, x)) >= 0.0 #Note: The above assumes that the drift is positive!  It is not checking the other t, though.
+    L_T = rho*I - Diagonal(mu_tilde.(T, x)) * L_1_plus - Diagonal(sigma_tilde.(T, x).^2/2) * L_2
+    u_T = L_T \ c_tilde.(T, x)
 
     function f(du,u,p,t)
-        #mu_tilde = -0.1+0.01*t+0.1*x#mu_tilde_T#-0.1+t+0.1*x
-        mu_tilde = mu_tilde_T#-0.1+t+0.1*x
-        L = Diagonal(rho*ones(M)) - (Diagonal(mu_tilde)*L_1_plus + Diagonal(sigma_tilde_T.^2/2)*L_2)
+        L = rho*I  - Diagonal(mu_tilde.(t, x)) * L_1_plus - Diagonal(sigma_tilde.(t, x).^2/2) * L_2
         A_mul_B!(du,L,u)
-        du .-= c_tilde
+        du .-= c_tilde.(t, x)
     end
 
-    tstops=(0.0, T)
-    prob = ODEProblem(f, u_T, tstops)
-    solve(prob, algorithm)
+    tspan=(0.0, T)
+    prob = ODEProblem(f, u_T, tspan)
+    if dt==nothing
+        solve(prob, algorithm)
+    else
+        solve(prob, algorithm, dt=dt, adaptive=adaptive)
+    end
 end
 
 x_min = 0.01
-x_max = 1
-M = 40
-T = 1.5
-N = 10 #This is not being used right now.
+x_max = 1.0
+M = 50
+T = 0.4
 rho = 0.05
 sigma_bar = 0.1
+c_tilde(t, x) = exp(x)
+sigma_tilde(t, x) =  sigma_bar * x
+mu_tilde(t, x) = 0.1*x + t/1500.0 #How is this so sensitive?
 #algorithm = Tsit5()
 #algorithm = AutoTsit5(Rosenbrock23())
+#algorithm = Rosenbrock23()
 algorithm = ImplicitEuler()
+#algorithm = Euler()
+dt = nothing #0.01#0.0001
+adaptive =true# true#false
 
-sol = time_stepping_example(x_min, x_max, M, T, N, rho, sigma_bar, algorithm)
-
+@time sol = time_stepping_example(c_tilde, sigma_tilde, mu_tilde, x_min, x_max, M, T, rho, algorithm, dt, adaptive)
+#time_stepping_example(x_min, x_max, M, T, N, rho, sigma_bar, algorithm)
 using Plots
 plot(sol, vars=1:5:M)
